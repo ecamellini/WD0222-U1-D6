64bca4031e523faed75c4447b4f072eb96cac3f3
Eric Camellini <eric.camellini@gmail.com>
2022-05-02 12:25:05 +0200

    Writing our first simple recipe

---
+++ recipe.js
@@ -0,0 +1,23 @@
+/*
+Welcome to the programming kitchen.
+An anlogy between baking cookies and programming.
+*/
+
+// Let's start by declaring our ingredients.
+let sugar = 120
+let butter = 80
+let flour = 200
+
+console.log(sugar)
+console.log(butter)
+console.log(flour)
+
+// In a medium bowl, mix together butter and sugar
+let mediumBowl
+console.log(mediumBowl)
+mediumBowl = butter + sugar
+console.log(mediumBowl)
+
+// Add flour to the mix, in the same bowl
+mediumBowl = mediumBowl + flour
+console.log(mediumBowl)
